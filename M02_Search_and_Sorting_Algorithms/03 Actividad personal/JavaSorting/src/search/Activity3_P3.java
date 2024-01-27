996d30b11da2202ffcb4ccffc48ce223abe5e8ef
Omar Acosta <omardil.acosta@gmail.com>
2024-01-27 09:40:03 -0600

    Change name ACT04 to ACT03

---
+++ M02_Search_and_Sorting_Algorithms/03 Actividad personal/JavaSorting/src/search/Activity3_P3.java
@@ -0,0 +1,25 @@
+package search;
+
+public class Activity3_P3 {
+
+	public static void main(String[] args) {
+
+		String[] test1 = {"the", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+		int[] results1 = findElement(test1, "the"); // Expected answer [0,5]
+		
+		//Add 4 more test cases for this program
+		
+	}
+	
+	/**
+	 * 
+	 * @param input Array of Strings containing a list of words
+	 * @param target String value to be searched
+	 * @return Array with the list of indexes where the "target" String can be found on array "input".
+	 */
+	public static int[] findElement(String[] input, String target) {
+		//Complete the code here
+		return null;
+	}
+
+}
