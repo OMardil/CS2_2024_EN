ebb3b7bc00895b13c1421ce642daaf6b141474da
Omar Acosta <omardil.acosta@gmail.com>
2024-01-27 09:15:02 -0600

    Add ACT4_P2 and ACT4_P3

---
+++ M02_Search_and_Sorting_Algorithms/03 Actividad personal/JavaSorting/src/search/Activity4_P2.java
@@ -0,0 +1,54 @@
+package search;
+
+public class Activity4_P2 {
+
+	public static void main(String[] args) {
+
+		char[] array1;
+		char[] array2; 
+
+		//Test Case 1
+		array1 = new char[]{'a', 'b', 'c'};
+		array2 = new char[]{'a', 'b', 'c'};
+		boolean p1 = compareArrays(array1, array2);
+		System.out.println("Test Case 1: Expected answer: true / Answer: " + p1);
+
+		//Test Case 2
+		array1 = new char[]{'a', 'b', 'c'};
+		array2 = new char[]{'a'};		
+		boolean p2 = compareArrays(array1, array2);
+		System.out.println("Test 2: Expected answer: false / Answer: " + p2);
+
+		//Test Case 3
+		array1 = new char[]{'a', 'b', 'c'};
+		array2 = new char[]{'A', 'B', 'C'};		
+		boolean p3 = compareArrays(array1, array2);
+		System.out.println("Test 3: Expected answer: false / Answer: " + p3);		
+		
+		//Test Case 4
+		array1 = new char[]{};;
+		array2 = new char[]{'a', 'b', 'c'};	
+		boolean p4 = compareArrays(array1, array2);
+		System.out.println("Test 4: Expected answer: false / Answer: " + p4);		
+		
+		//Test Case 5
+		array1 = new char[]{'a', 'b', 'c', 'd'};;
+		array2 = new char[]{'a', 'b', 'c'};	
+		boolean p5 = compareArrays(array1, array2);
+		System.out.println("Test 5: Expected answer: false / Answer: " + p5);			
+		
+	}
+	
+	/**
+	 * Compares array a with array b. The result is true if and only if the 
+	 * size of both arrays is the same, and the contents of each index are equal.
+	 * @param a
+	 * @param b
+	 * @return True if both arrays are equal, false if they are different.
+	 */
+	public static boolean compareArrays(char[] a, char[] b){
+		//complete code here
+		return true;
+	}
+
+}
---
+++ M02_Search_and_Sorting_Algorithms/03 Actividad personal/JavaSorting/src/search/Activity4_P3.java
@@ -0,0 +1,22 @@
+package search;
+
+public class Activity4_P3 {
+
+	public static void main(String[] args) {
+
+		//Complete a set of 4 test cases for this program
+		
+	}
+	
+	/**
+	 * 
+	 * @param input Array of Strings containing a list of words
+	 * @param target String value to be searched
+	 * @return Array with the list of indexes where the "target" String can be found on array "input".
+	 */
+	public static int[] findElement(String[] input, String target) {
+		//Complete the code here
+		return null;
+	}
+
+}
